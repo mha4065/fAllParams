e548ad4f4b8ae29fc43683e00850c2eb7fc05eff
mha4065 <mha4065@protonmail.com>
2023-06-18 10:33:15 +0330

    fixed

--- modules/json_function.py
+++ modules/json_function.py
@@ -1,35 +1,23 @@
 from json import loads as json_loads
 
-# Get all keys from JSON content-type
-def get_keys(data):
-    params = []
-    for key in data.keys():
-        params.append(key)
-        if isinstance(data[key], dict):
-            for sub_key in data[key].keys():
-                params.append(sub_key)
-                if isinstance(data[key][sub_key], dict):
-                    for sub_sub_key in data[key][sub_key].keys():
-                        params.append(sub_sub_key)
-                        if isinstance(data[key][sub_key][sub_sub_key], dict):
-                            for sub_sub_sub_key in data[key][sub_key][sub_sub_key].keys():
-                                params.append(sub_sub_sub_key)
-        elif isinstance(data[key], list):
-            for item in data[key]:
-                if isinstance(item, dict):
-                    for sub_key in item.keys():
-                        params.append(sub_key)
-                        if isinstance(item[sub_key], dict):
-                            for sub_sub_key in item[sub_key].keys():
-                                params.append(sub_sub_key)
-                                if isinstance(item[sub_key][sub_sub_key], dict):
-                                    for sub_sub_sub_key in item[sub_key][sub_sub_key].keys():
-                                        params.append(sub_sub_sub_key)
-    params = list(set(params))
-    return params
+
+def extract_keys(obj):
+    keys = []
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            keys.append(key)
+            keys.extend(extract_keys(value))
+    elif isinstance(obj, list):
+        for item in obj:
+            keys.extend(extract_keys(item))
+    return keys
 
 
 def json_params(response):
-    data = json_loads(response.content)
-    params = get_keys(data)
-    return params
+    try:
+        data = json_loads(response.content)
+        keys = extract_keys(data)
+        keys = list(set(keys))
+        return keys
+    except ValueError:
+        pass
